9e7df02f51d9e72c781e7888bcf960fcb555d906
Petra <120021800+korhone@users.noreply.github.com>
2024-03-08 13:11:04 +0200

    Add solution

---
+++ Auto/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarDealership.view;
+
+namespace CarDealership
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainMenu MainWindow = new MainMenu();
+            MainWindow.ShowDialog();
+        }
+    }
+}
